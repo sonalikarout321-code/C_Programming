79d358722bb4a5f7920dbb8df6fbc8b74989f49b
sonalikarout321-code <Sonalikarout321@gmail.com>
2026-02-10 01:31:11 +0530

    my first commit hello world program

---
+++ Helloworld.c
@@ -0,0 +1,29 @@
+//WAP to print Heloo world emti title dia jae
+#include<stdio.h>
+int main(){
+    printf("Hello World");
+    return 0;
+}
+//WAP to print my name
+#include<stdio.h>
+int main(){
+    printf("Name: Sonalika Rout\n");
+    printf("Age:19\n");
+    printf("Address:Cuutack ,Kandarpur\n");
+    return 0;
+}
+//WAP to print addition of two numbers
+#include<stdio.h>
+int main(){
+    int a=5,b=7;
+    printf("sum=%d",a+b);
+    return 0;
+}
+// hau emti title daba agaku gote file re gote code rahiba au ta file name bi semti daba jemti hello world dia heichi
+// first repo url init kara gala
+// git remote add origin https://github.com/sonalikarout321-code/C_Programming.git
+// ta pare git init
+// ta pare git status check karibaku ki keteta file ame change karichu au psuh karibu
+// ta pare git add .
+// ta pare git commit -m "my first commit c program hello world program"
+// ta pare fiannly git push
